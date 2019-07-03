8ad597b1f9177b4076bb0c7425130504ee1987a3
Aaron Carlino <unclecheese@leftandmain.com>
2019-07-04 11:16:51 +1200

    Remove unused imports

--- app/src/Page.php
+++ app/src/Page.php
@@ -2,13 +2,7 @@
 
 namespace {
 
-    use DNADesign\Elemental\Extensions\ElementalPageExtension;
-    use DNADesign\Elemental\Models\ElementalArea;
-    use SilverStripe\Bambusa\Blocks\CarouselBlock;
     use SilverStripe\CMS\Model\SiteTree;
-    use SilverStripe\ElementalBannerBlock\Block\BannerBlock;
-    use SilverStripe\Forms\HeaderField;
-    use Symbiote\GridFieldExtensions\GridFieldAddNewMultiClass;
 
     class Page extends SiteTree
     {

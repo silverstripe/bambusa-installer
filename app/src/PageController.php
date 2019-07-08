11244959ddefb990120ce050c355abeab4164d76
Maxime Rainville <maxime@silverstripe.com>
2019-07-08 14:48:35 +1200

    Move all the show modal cookie logic client side.

--- app/src/PageController.php
+++ app/src/PageController.php
@@ -3,34 +3,17 @@
 namespace {
 
     use SilverStripe\CMS\Controllers\ContentController;
-    use SilverStripe\Control\Cookie;
-    use SilverStripe\Control\Director;
-    use SilverStripe\Control\Session;
-    use SilverStripe\Core\Environment;
     use SilverStripe\View\ArrayData;
     use SilverStripe\View\Requirements;
 
     class PageController extends ContentController
     {
-
-        /**
-         * Whether our modal should be showed on this request.
-         * @var bool
-         */
-        private $showModal = false;
-
         protected function init()
         {
             parent::init();
-
             // Inject our privacy modal assets into the page
-            $cookie = Cookie::get('privacy_modal_showed');
-            if (empty($cookie)) {
-                $this->showModal = true;
-                Requirements::customScript('var privacy_modal_hostname=' . json_encode(Director::host()));
-                Requirements::javascript('app/js/dialog.js', ['defer' => true]);
-                Requirements::css('app/css/dialog.css');
-            }
+            Requirements::javascript('app/js/dialog.js', ['defer' => true]);
+            Requirements::css('app/css/dialog.css');
         }
 
         /**
@@ -39,9 +22,7 @@ namespace {
          */
         public function ModalWindow()
         {
-            if ($this->showModal) {
-                return ArrayData::create([])->renderWith('PrivacyModal') ;
-            }
+            return ArrayData::create([])->renderWith('PrivacyModal');
         }
     }
 }

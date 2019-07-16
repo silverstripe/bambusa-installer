93e60b2719e0ea13c3232c04864f3ec5d0bb96e7
Aaron Carlino <unclecheese@leftandmain.com>
2019-07-16 13:28:19 +1200

    Editor.css fixes

--- app/_config.php
+++ app/_config.php
@@ -2,8 +2,34 @@
 
 use SilverStripe\Security\PasswordValidator;
 use SilverStripe\Security\Member;
+use SilverStripe\Forms\HTMLEditor\TinyMCEConfig;
 
 // remove PasswordValidator for SilverStripe 5.0
 $validator = PasswordValidator::create();
 // Settings are registered via Injector configuration - see passwords.yml in framework
 Member::set_password_validator($validator);
+$formats =  [
+    [
+       'title' => 'Lead text (bigger)',
+       'selector' => 'p',
+       'classes' => 'lead',
+   ],
+   [
+       'title' => '“Quote”',
+       'selector' => 'p',
+       'classes' => 'quote',
+   ],
+   [
+       'title' => 'Button',
+       'selector' => 'a',
+       'classes' => 'btn btn-primary',
+   ],
+];
+TinyMCEConfig::get('cms')
+    ->setOptions([
+        'body_class' => '',
+        'importcss_append' => true,
+        'style_formats' => $formats,
+        'importcss_selector_filter' => ' ', // hide all selectors
+    ])
+    ->insertButtonsAfter('formatselect', 'styleselect');

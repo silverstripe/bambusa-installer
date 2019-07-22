46abdddcf858cd4cd486c11b6ef44a86f10902cb
Aaron Carlino <unclecheese@leftandmain.com>
2019-07-22 20:41:59 +1200

    Custom focus keyword analysis to allow for blocks

---
+++ app/src/SEO/FocusKeywordContentAnalysis.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace SilverStripe\Bambusa\SEO;
+
+use Vulcan\Seo\Analysis\FocusKeywordContentAnalysis as BaseAnalysis;
+use Vulcan\Seo\Seo;
+
+class FocusKeywordContentAnalysis extends BaseAnalysis
+{
+    /**
+     * @return string
+     */
+    public function getContentFromDom()
+    {
+        $content = Seo::collateContentFields($this->getPage());
+        return strtolower(strip_tags($content ?: ''));
+    }
+
+}

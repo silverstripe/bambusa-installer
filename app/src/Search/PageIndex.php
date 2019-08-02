a78a73c3ad323a0f3e2f1421cbe508a45a072910
Ingo Schommer <me@chillu.com>
2019-08-02 17:05:48 +1200

    Don't index draft content
    It's a security issue waiting to happen,
    and doubles our indexing time (and time-to-demo-ready)

--- app/src/Search/PageIndex.php
+++ app/src/Search/PageIndex.php
@@ -5,7 +5,9 @@ namespace SilverStripe\Bambusa\Search;
 
 use SilverStripe\Bambusa\Pages\BlocksPage;
 use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\FullTextSearch\Search\Variants\SearchVariantVersioned;
 use SilverStripe\FullTextSearch\Solr\SolrIndex;
+use SilverStripe\Versioned\Versioned;
 
 class PageIndex extends SolrIndex
 {
@@ -18,5 +20,7 @@ class PageIndex extends SolrIndex
         $this->addFulltextField('Title', null, ['boost' => 2]);
         $this->addFulltextField('ElementsForSearch');
         $this->addStoredField('Link');
+
+        $this->excludeVariantState([SearchVariantVersioned::class => Versioned::DRAFT]);
     }
 }

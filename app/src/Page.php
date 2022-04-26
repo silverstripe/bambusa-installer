2ca5cc50321c29ecc9cb189c3f77dc10f16d7c61
Jackson Darlow <jackson.e.darlow@gmail.com>
2022-04-26 17:15:26 +1200

    Added codecraft/silverstripe-pathfinder * Added Taxonomy settings to Pages

--- app/src/Page.php
+++ app/src/Page.php
@@ -3,17 +3,45 @@
 namespace {
 
     use SilverStripe\CMS\Model\SiteTree;
+    use SilverStripe\Forms\CheckboxField;
     use SilverStripe\Forms\Tab;
     use SilverStripe\Forms\ToggleCompositeField;
+    use SilverStripe\Forms\TreeMultiselectField;
+    use SilverStripe\Taxonomy\TaxonomyTerm;
 
     class Page extends SiteTree
     {
+        /**
+         * @var string[]
+         */
+        private static $many_many = [
+            'Terms' => TaxonomyTerm::class,
+        ];
+
         public function getCMSFields()
         {
             $fields = parent::getCMSFields();
 
-            // Not something we want users playing with.
-            $fields->removeByName('ExtraMeta');
+            $fields->removeByName([
+                'Terms',
+                'ExtraMeta' // Not something we want users playing with
+            ]);
+
+            return $fields;
+        }
+
+        public function getSettingsFields()
+        {
+            $fields =  parent::getSettingsFields();
+
+            $fields->addFieldToTab(
+                'Root.Taxonomy',
+                TreeMultiselectField::create(
+                    'Terms',
+                    _t(__CLASS__ . '.Terms', 'Terms'),
+                    TaxonomyTerm::class
+                )
+            );
 
             return $fields;
         }
